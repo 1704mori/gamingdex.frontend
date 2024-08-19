07ad114043941fdd8d775b97a4d25641440a108d
1704mori <1704mori@gmail.com>
2024-08-19 15:32:13 -0300

    wip header search bar

--- components/header.tsx
+++ components/header.tsx
@@ -22,10 +22,33 @@ import { useAtom } from "jotai";
 import { userAtom } from "@/lib/stores/user";
 import { Dialog, DialogTrigger } from "./ui/dialog";
 import EditUserDialog from "./updateuser";
-import { deleteCookie } from "@/lib/utils";
+import { API_URL, deleteCookie, shimmer, toBase64 } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { GameType } from "@/lib/types/game";
+import { useEffect, useState } from "react";
+import { useDebounceValue } from "@/lib/hooks/useDebounceValue";
+import Image from "next/image";
 
 export default function Header() {
   const [user] = useAtom(userAtom);
+
+  const [search, setSearch] = useState("");
+  const [searchInput, setSearchInput] = useState(search);
+
+  const debouncedSearch = useDebounceValue(searchInput, 300); // 300ms delay
+
+  useEffect(() => {
+    setSearch(debouncedSearch);
+  }, [debouncedSearch, setSearch]);
+
+  const { data: games, isLoading } = useQuery({
+    queryKey: ["games_header", search],
+    queryFn: async () => {
+      const response = await fetch(`${API_URL}/games?search=${search}`);
+      return response.json<GameType[]>();
+    },
+  });
+
   return (
     <header className="flex justify-between items-center py-6 px-4 md:px-6 lg:px-8 bg-neutral-900 text-neutral-50 dark:bg-neutral-950 dark:text-neutral-50">
       <div className="flex items-center">
@@ -48,8 +71,48 @@ export default function Header() {
       </div>
       <div className="flex items-center gap-4">
         <div className="relative min-w-72">
-          <Input className="w-full" placeholder="Search" />
+          <Input
+            className="w-full"
+            placeholder="Search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          {search && (
+            <div className="absolute w-full top-14 left-0 border border-neutral-200 rounded-lg shadow-sm bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-900 overflow-hidden group p-4">
+              {isLoading ? (
+                <p>loading</p>
+              ) : (
+                <>
+                  {games?.attributes?.map((game) => (
+                    <div
+                      className="relative border border-neutral-200 rounded-lg shadow-sm bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-900 overflow-hidden group"
+                      key={game.id}
+                    >
+                      <Link
+                        href={`/game/${game.id}`}
+                        className="block relative h-72"
+                      >
+                        <Image
+                          className="object-cover"
+                          alt={game.title}
+                          src={game.cover_url!}
+                          placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(500, 300))}`}
+                          fill
+                        />
+                        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-75 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                          <h3 className="text-white text-center text-lg font-semibold">
+                            {game.title}
+                          </h3>
+                        </div>
+                      </Link>
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
+          )}
         </div>
+
         <div className="flex items-center">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -61,7 +124,7 @@ export default function Header() {
                 {user?.id && <p>{user?.username}</p>}
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
+            <DropdownMenuContent className="p-6 mr-12 mt-4">
               {user?.id && (
                 <>
                   <DropdownMenuItem>
